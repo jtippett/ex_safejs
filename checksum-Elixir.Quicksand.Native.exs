0a60e482224a45b3a7ee103614f168629f8c637e
Louis-Philippe Gauthier <lpgauth@gmail.com>
2026-05-14 15:58:08 -0400

    Add checksums for v0.1.1

--- checksum-Elixir.Quicksand.Native.exs
+++ checksum-Elixir.Quicksand.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libquicksand-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a9da650c873f19a0601229a902da19dbd6da9e76669ff7d5b6a7e9ae1c53fe7e",
-  "libquicksand-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a53cf1d016755041ce23555357c2a620307705944d35b267c11c02f9a4eae74b",
-  "libquicksand-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ddc87f28207973d8855df0b727db5727867c1524153ce570dd98d3e942226e1f",
-  "libquicksand-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d6a401753e39868b379d4f22577c7baca4677f718f983265e4cdb3a42cab5b7b",
+  "libquicksand-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:61ad1675b2bb2e3d86498b325e870294bfb24a76545af7e3a6a1c341edd1aa04",
+  "libquicksand-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:eda0c65c4721e3607a5ff8b779b06bc2a2a9fa02695d9b2be114cb079c9a2110",
+  "libquicksand-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:e1a3fccd8f9911ce0e79d868df8fb16603b43593f12f7cada62ca1d083aa2190",
+  "libquicksand-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:1db70c306733950c8b2b7d3721a458a3996acca4773ee5acc60ed961a59a66b5",
 }
