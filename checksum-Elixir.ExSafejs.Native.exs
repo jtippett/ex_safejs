a998fdf4c2ee45b75893e74daa67d0dc5641da0a
James Tippett <james.tippett@gmail.com>
2026-08-19 18:38:46 +0700

    Add checksums for v0.3.0

--- checksum-Elixir.ExSafejs.Native.exs
+++ checksum-Elixir.ExSafejs.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libex_safejs-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:526483192261c04710c458be519b4b0f6c27b8b5b90f6b9c6f2949c8f1a99b42",
-  "libex_safejs-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dda799514b23820090d7dd11f1a09178f24311cfe3620be24a51d556973b0c34",
-  "libex_safejs-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:36048edefe6ced674f7996ef9f878d8914bf791cc42c6ab35e2b9816f1eb6e98",
-  "libex_safejs-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9cd84a611cba7f68f774b75e5d67ef4a928fbdc691fca289f86aec0b32ff5c45",
+  "libex_safejs-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:0f8f730ae05d8fa7df71e8d7be84054124d4faba1d4b270bf2e570e53b264267",
+  "libex_safejs-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:387430d84f996c262d110bd5352a3efafc4c83f33f9dd0cb6ed79a1e1470191c",
+  "libex_safejs-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8f363e3725cedfecdb26c905b17b21db1ff8c80d38a240140b00438d21f00595",
+  "libex_safejs-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:61e0a832b4ece8c6884bbff43c5d21bb81ab63b48198c42d2e8deb3eac6642a8",
 }
