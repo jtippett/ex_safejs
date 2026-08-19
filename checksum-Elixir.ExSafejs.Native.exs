5a2331a098549c695cd4e8921052fa285e15065b
James Tippett <james.tippett@gmail.com>
2026-08-19 17:14:29 +0700

    Add checksums for v0.2.0

---
+++ checksum-Elixir.ExSafejs.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "libex_safejs-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:526483192261c04710c458be519b4b0f6c27b8b5b90f6b9c6f2949c8f1a99b42",
+  "libex_safejs-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dda799514b23820090d7dd11f1a09178f24311cfe3620be24a51d556973b0c34",
+  "libex_safejs-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:36048edefe6ced674f7996ef9f878d8914bf791cc42c6ab35e2b9816f1eb6e98",
+  "libex_safejs-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9cd84a611cba7f68f774b75e5d67ef4a928fbdc691fca289f86aec0b32ff5c45",
+}
